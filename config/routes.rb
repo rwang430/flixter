5bd10f69829a3b44353321fce74f5a268c2fc3ad
Roger Wang <blumoo@gmail.com>
2017-03-20 08:20:55 +0000

    made changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
