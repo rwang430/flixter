0182b2993058247623ca50a7dcc7e00666015742
Roger Wang <blumoo@gmail.com>
2017-04-06 00:01:37 +0000

    video upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,20 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.storage      = :aws
-  config.aws_bucket   = ENV["AWS_BUCKET"]
-  config.aws_acl      = "public-read"
+  config.storage    = :aws
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl    = "public-read"
 
-  config.fog_credentials = {
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],                        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],                         # required
-    region:                ENV["AWS_REGION"]
+  config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"]
   }
+
+  if Rails.env.development?
+  config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+  config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
   
 end
+
